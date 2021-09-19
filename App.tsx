e91759546e44e5db983d0376ac1141c8f6cc7f70
Caleb Mathew <creatrixity@gmail.com>
2021-09-20 00:54:00 +0100

    feat: add custom theme for button

--- App.tsx
+++ App.tsx
@@ -2,15 +2,17 @@ import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { Provider as PaperProvider, Button } from "react-native-paper";
 import { StatusBar } from "expo-status-bar";
+import DefaultTheme from "./src/config/theme/default";
 
 export default function App() {
   return (
-    <PaperProvider>
+    <PaperProvider theme={DefaultTheme}>
       <View style={styles.container}>
         <Text>Open up App.tsx to start working on your app!</Text>
         <Button
           icon="camera"
           mode="contained"
+          labelStyle={{ color: DefaultTheme.colors.accent }}
           onPress={() => console.log("Pressed")}
         >
           Press me
---
+++ src/config/theme/default.ts
@@ -0,0 +1,12 @@
+import { DefaultTheme } from "react-native-paper";
+
+const theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: "#A8DADC",
+    accent: "#1D3557",
+  },
+};
+
+export default theme;
