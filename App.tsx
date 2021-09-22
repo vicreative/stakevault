dc12231cb7ed024b2209b47c0f32bcb1efe8a2ba
Victoria Ogbiyoyo <vogbiyoyo@gmail.com>
2021-09-22 19:14:11 +0100

    feat(welcome-screen): add title. illustration & btn

--- App.tsx
+++ App.tsx
@@ -1,32 +1,42 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import { Provider as PaperProvider, Button } from 'react-native-paper';
+import { Provider as PaperProvider } from 'react-native-paper';
 import { StatusBar } from 'expo-status-bar';
-import DefaultTheme from 'shared/config/theme/default';
+import theme from 'shared/config/theme/default';
+import AppLoading from 'expo-app-loading';
+import {
+  useFonts,
+  Montserrat_400Regular,
+  Montserrat_600SemiBold,
+  Montserrat_700Bold,
+} from '@expo-google-fonts/montserrat';
+import { Welcome } from 'features/Welcome';
 
 export default function App() {
-  return (
-    <PaperProvider theme={DefaultTheme}>
-      <View style={styles.container}>
-        <Text>Open up App.tsx to start working on your app!</Text>
-        <Button
-          icon="camera"
-          mode="contained"
-          labelStyle={{ color: DefaultTheme.colors.accent }}
-          onPress={() => console.log('Pressed')}
-        >
-          Press me
-        </Button>
-        <StatusBar style="auto" />
-      </View>
-    </PaperProvider>
-  );
+  let [fontsLoaded] = useFonts({
+    Montserrat_600SemiBold,
+    Montserrat_400Regular,
+    Montserrat_700Bold,
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  } else {
+    return (
+      <PaperProvider theme={theme}>
+        <View style={styles.container}>
+          <Welcome />
+
+          <StatusBar style="auto" />
+        </View>
+      </PaperProvider>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
--- src/shared/config/theme/default.ts
+++ src/shared/config/theme/default.ts
@@ -1,11 +1,35 @@
-import { DefaultTheme } from "react-native-paper";
+import { configureFonts, DefaultTheme } from 'react-native-paper';
+
+const fontConfig: any = {
+  web: {
+    regular: {
+      fontFamily: 'Montserrat_400Regular',
+      fontWeight: '400',
+    },
+    semibold: {
+      fontFamily: 'Montserrat_600Semibold',
+      fontWeight: '600',
+    },
+  },
+  android: {
+    regular: {
+      fontFamily: 'Montserrat_400Regular',
+      fontWeight: '400',
+    },
+    bold: {
+      fontFamily: 'Montserrat_700Bold',
+      fontWeight: '700',
+    },
+  },
+};
 
 const theme = {
   ...DefaultTheme,
+  fonts: configureFonts(fontConfig),
   colors: {
     ...DefaultTheme.colors,
-    primary: "#A8DADC",
-    accent: "#1D3557",
+    primary: '#A8DADC',
+    accent: '#1D3557',
   },
 };
 
