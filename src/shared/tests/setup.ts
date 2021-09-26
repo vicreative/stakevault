adbd79dca5716e5af41d25a5dce3bfdf684d3bac
Victoria Ogbiyoyo <vogbiyoyo@gmail.com>
2021-09-26 11:47:21 +0100

    feat(register-screen): add react-navigation mock

---
+++ src/shared/tests/setup.ts
@@ -0,0 +1,12 @@
+const mockedDispatch = jest.fn();
+
+jest.mock('@react-navigation/native', () => {
+  const actualNav = jest.requireActual('@react-navigation/native');
+  return {
+    ...actualNav,
+    useNavigation: () => ({
+      navigate: jest.fn(),
+      dispatch: mockedDispatch,
+    }),
+  };
+});
